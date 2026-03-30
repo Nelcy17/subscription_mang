60bfbbc0c0822a7dcb795a0e6e29137fc53a897c
nelcy17 <nelcy.rathore07@gmail.com>
2026-03-30 22:57:06 +0530

    updated config file

--- config/arcjet.js
+++ config/arcjet.js
@@ -1,23 +1,13 @@
-import arcjet, { shield, detectBot, tokenBucket } from "@arcjet/node";
-import { ARCJET_KEY } from './env.js'
+import arcjet, { tokenBucket } from "@arcjet/node";
 
 const aj = arcjet({
-  key: ARCJET_KEY,
-  characteristics: ["ip.src"],
+  key: process.env.ARCJET_KEY,
   rules: [
-    shield({ mode: "LIVE" }),
-    detectBot({
-      mode: "LIVE",
-      allow: [ "CATEGORY:SEARCH_ENGINE",
-              "CATEGORY:API_CLIENT",
-              "USER_AGENT:Postman"  
-       ],
-    }),
     tokenBucket({
       mode: "LIVE",
-      refillRate: 5, // Refill 5 tokens per interval
-      interval: 10, // Refill every 10 seconds
-      capacity: 10, // Bucket capacity of 10 tokens
+      refillRate: 5,     // tokens per second
+      interval: 1,
+      capacity: 10,      // burst allowed
     }),
   ],
 });
